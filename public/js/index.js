bf7714497053da6a68ac240e1a038486b6f95f3c
Franco Carini <carini.franco@gmail.com>
2020-09-26 17:39:19 -0300

    Add Change Password functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,14 @@
 import '@babel/polyfill'
 import { login, logout } from './login'
 import { displayMap } from './mapbox'
-import { updateData } from './updateSettings'
+import { updateSettings } from './updateSettings'
 
 // DOM Elements
 const mapBox = document.getElementById('map')
 const loginForm = document.querySelector('.form--login')
 const logoutBtn = document.querySelector('.nav__el--logout')
 const userDataForm = document.querySelector('.form-user-data')
+const userPasswordForm = document.querySelector('.form-user-settings')
 
 if (mapBox) {
   const locations = JSON.parse(mapbox.dataset.locations)
@@ -28,10 +29,27 @@ if (logoutBtn) {
 }
 
 if (userDataForm) {
-  userDataForm.addEventListener('submit', e => {
+  userDataForm.addEventListener('submit', async e => {
     e.preventDefault()
     const name = document.getElementById('name').value
     const email = document.getElementById('email').value
-    updateData(name, email)
+    await updateSettings({name, email}, 'data')
+  })
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault()
+    document.querySelector('.btn--save-password').textContent = 'Updating...'
+
+    const passwordCurrent = document.getElementById('password-current').value
+    const password = document.getElementById('password').value
+    const passwordConfirm = document.getElementById('password-confirm').value
+    await updateSettings({passwordCurrent, password, passwordConfirm}, 'password')
+
+    document.querySelector('.btn--save-password').textContent = 'Save Password'
+    document.getElementById('password-current').value = ''
+    document.getElementById('password').value = ''
+    document.getElementById('password-confirm').value = ''
   })
 }
